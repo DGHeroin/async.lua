7fe25e9c4a1d16da856b2283d78b7bc72f7f6e90
Blake Li <6779586+DGHeroin@users.noreply.github.com>
2020-05-22 15:32:52 +0800

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,23 @@
+local async = require('async')
+
+function step1(a, b, c, next)
+    print('step1', a, b, c)
+    -- you may call the in some callback
+    async.next(next)
+end
+
+local function step2(a, b, c, next)
+    print('step2', a, b, c)
+    async.next(next)
+end
+
+local function step3(a, b, c, next)
+    print('step3', a, b, c)
+    async.next(next)
+end
+
+
+async.call(
+    {step1, 1, 2, 3},
+    {step2, 4, 5, 6},
+    {step3, 7, 8, 9})
