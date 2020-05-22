63d42314e09141189e02d511a4b2351c88cd858b
Blake Li <6779586+DGHeroin@users.noreply.github.com>
2020-05-22 15:55:01 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,5 +1,6 @@
 local async = require('async')
 
+---- async call ----
 function step1(a, b, c, next)
     print('step1', a, b, c)
     -- you may call the in some callback
@@ -16,8 +17,29 @@ local function step3(a, b, c, next)
     async.next(next)
 end
 
-
 async.call(
     {step1, 1, 2, 3},
     {step2, 4, 5, 6},
     {step3, 7, 8, 9})
+
+---- water fall ----
+async.waterfall({
+    function (callback)
+        callback(nil, 1, 2)
+    end,
+    function (callback, a, b)
+        print(a, b)
+        callback(nil, 3)
+    end,
+    function(callback, a)
+        print(a)
+        callback(nil, 4)
+    end
+    }, 
+    function (err, result)
+        if err then
+            print('error:', err)
+        else
+            print(result)
+        end
+end)
